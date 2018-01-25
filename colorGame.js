82976836b79f5d38619c050b4dd2472cf33bfc40
bhag41 <35288783+bhag41@users.noreply.github.com>
2018-01-25 13:55:32 -0800

    clickeventlistener

--- colorGame.js
+++ colorGame.js
@@ -6,4 +6,23 @@ var colors = [
  "rgb(0, 0, 255)",
  "rgb(255, 0, 255)",
 ]
-var squares = document.querySelectorAll(".square")
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (i = 0; i<squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//add click listener
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor;
+		if (clickedColor === pickedColor)
+		{
+			alert("Correct!!");
+		}
+		else { alert ("wrong!!");}
+	});
+}
