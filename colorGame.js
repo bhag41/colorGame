702c2c66e4d210dee5ae9dd6a21e41a29187c52e
bhag41 <35288783+bhag41@users.noreply.github.com>
2018-01-25 19:57:52 -0800

    almost done

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,33 @@
-var colors = [ 
- "rgb(255, 0, 0)",
- "rgb(255, 255, 0)",
- "rgb(0, 255, 0)",
- "rgb(0, 255, 255)",
- "rgb(0, 0, 255)",
- "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+
+});
+hardBtn.addEventListener("click", function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+})
+
+resetButton.addEventListener("click", function(){
+	resetButton.textContent = "New Colors";
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];	
+	}
+	h1.style.backgroundColor = "#232323"
+})
 colorDisplay.textContent = pickedColor;
 
 for (i = 0; i<squares.length; i++)
@@ -25,6 +42,8 @@ for (i = 0; i<squares.length; i++)
 		{
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+			resetButton.textContent = "Play Again?";
 
 		}
 		else { 
@@ -49,3 +68,18 @@ function pickColor(){
 
 }
 	
+function generateRandomColors (num){
+	var arr = [];
+	for (var i = 0; i<num; i++){
+		arr.push(randomColor());
+
+	}
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + "," + "\ " + g + "," + "\ " + b + ")";
+}
